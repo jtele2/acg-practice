957a8968fe83d49d3cad5602038407cea0f1dde8
Ubuntu <ubuntu@ip-172-31-11-122.ec2.internal>
2024-02-25 08:58:27 +0000

    working on ec2

---
+++ kinesis/colored_loggers.py
@@ -0,0 +1,87 @@
+import logging
+import json
+import os
+
+
+class PersistentDynamicColorFormatter(logging.Formatter):
+    storage_file = "logger_colors.json"
+    # A set of predefined, easily distinguishable colors.
+    distinct_colors = [
+        "\033[31m",  # Red
+        "\033[32m",  # Green
+        "\033[33m",  # Yellow
+        "\033[34m",  # Blue
+        "\033[35m",  # Magenta
+        "\033[36m",  # Cyan
+        "\033[91m",  # Light Red
+        "\033[92m",  # Light Green
+        "\033[93m",  # Light Yellow
+        "\033[94m",  # Light Blue
+        "\033[95m",  # Light Magenta
+        "\033[96m",  # Light Cyan
+        "\033[37m",  # White
+        "\033[97m",  # Bright White
+        "\033[90m",  # Bright Black (Gray)
+        "\033[30m",  # Black
+        "\033[38;5;208m",  # Orange
+        "\033[38;5;202m",  # Red Orange
+        "\033[38;5;214m",  # Yellow Orange
+        "\033[38;5;226m",  # Bright Yellow
+        "\033[38;5;10m",  # Bright Green
+        "\033[38;5;46m",  # Sea Green
+        "\033[38;5;14m",  # Bright Cyan
+        "\033[38;5;49m",  # Dark Cyan
+        "\033[38;5;27m",  # Deep Blue
+        "\033[38;5;21m",  # Royal Blue
+        "\033[38;5;57m",  # Neon Blue
+        "\033[38;5;93m",  # Purple
+        "\033[38;5;201m",  # Bright Magenta
+        "\033[38;5;198m",  # Bright Pink
+        "\033[38;5;163m",  # Medium Pink
+        "\033[38;5;129m",  # Lavender
+        "\033[38;5;165m",  # Peach
+        "\033[38;5;9m",  # Darker Red
+        "\033[38;5;161m",  # Dark Magenta
+        "\033[38;5;13m",  # Medium Magenta
+        "\033[38;5;221m",  # Light Gold
+        "\033[38;5;178m",  # Light Brown
+        "\033[38;5;3m",  # Olive
+        "\033[38;5;11m",  # Dark Yellow
+        "\033[38;5;19m",  # Navy Blue
+        "\033[38;5;141m",  # Light Lavender
+        "\033[38;5;171m",  # Sky Blue
+        "\033[38;5;123m",  # Light Blue
+        "\033[38;5;159m",  # Light Cyan
+        "\033[38;5;105m",  # Soft Purple
+        "\033[38;5;69m",  # Turquoise
+        "\033[38;5;150m",  # Aqua
+        "\033[38;5;85m",  # Medium Blue
+        "\033[38;5;39m",  # Dark Cyan
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.load_colors()
+
+    def load_colors(self):
+        if os.path.exists(self.storage_file):
+            with open(self.storage_file, "r") as file:
+                self.logger_colors = json.load(file)
+        else:
+            self.logger_colors = {}
+
+    def save_colors(self):
+        with open(self.storage_file, "w") as file:
+            json.dump(self.logger_colors, file)
+
+    def format(self, record):
+        if record.name not in self.logger_colors:
+            # Cycle through the distinct colors list as needed
+            color_index = len(self.logger_colors) % len(self.distinct_colors)
+            self.logger_colors[record.name] = self.distinct_colors[color_index]
+            self.save_colors()
+
+        color_code = self.logger_colors[record.name]
+        formatted_message = super().format(record)
+        colored_message = f"{color_code}{formatted_message}\033[0m"
+        return colored_message
--- kinesis/kinesis_producer.py
+++ kinesis/kinesis_producer.py
@@ -7,30 +7,50 @@ import json
 import requests
 import logging
 import boto3
+from colored_loggers import PersistentDynamicColorFormatter
 
-logging.basicConfig(level=logging.DEBUG)
+# Configure logging to use the enhanced persistent dynamic color formatter
+handler = logging.StreamHandler()
+formatter = PersistentDynamicColorFormatter("%(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logging.basicConfig(level=logging.DEBUG, handlers=[handler])
 
-# Get data from randomuser.me
-while True:
-    random_user = requests.get("https://randomuser.me/api").json()["results"][0]
-
-    if random_user["dob"]["age"] >= 21:
-        random_user_trim = {
-            "fname": random_user["name"]["first"],
-            "lname": random_user["name"]["last"],
-            "age": random_user["dob"]["age"],
-            "gender": random_user["gender"],
-            "latitude": random_user["coordinates"]["latitude"],
-            "longitude": random_user["coordinates"]["longitude"],
-        }
-        break
-    logging.info("user under 21")
-
-# test_data = {"name": "foo", "last_name": "bar"}
-test_data_enc = (json.dumps(random_user_trim) + "\n").encode()
+STREAM_NAME = "put-s3-j7irn"
 
 client = boto3.client("firehose", region_name="us-east-1")
-response = client.put_record(
-    DeliveryStreamName="put-s3-j7irn-test", Record={"Data": test_data_enc}
-)
-print(json.dumps(response, indent=2))
+
+# Get 100 data records from randomuser.me
+i = 0
+while i < 1000:
+    while True:
+        random_user = requests.get("https://randomuser.me/api").json()["results"][0]
+        username = f"{random_user['name']['first']} {random_user['name']['last']}"
+        if (random_user["dob"]["age"] >= 21) and (
+            random_user["location"].get("coordinates") is not None
+        ):
+            i += 1
+            break
+        elif random_user["dob"]["age"] < 21:
+            logging.info(f"User {username} has age <21")
+        elif random_user["location"].get("coordinates") is None:
+            logging.info(f"User {username} has no coordinates")
+        else:
+            raise ValueError
+
+    random_user_trim = {
+        "fname": random_user["name"]["first"],
+        "lname": random_user["name"]["last"],
+        "age": random_user["dob"]["age"],
+        "gender": random_user["gender"],
+        "latitude": random_user["location"]["coordinates"]["latitude"],
+        "longitude": random_user["location"]["coordinates"]["longitude"],
+    }
+
+    # test_data = {"name": "foo", "last_name": "bar"}
+    # Data Firehose requires binary
+    test_data_enc = json.dumps(random_user_trim).encode()
+
+    response = client.put_record(
+        DeliveryStreamName=STREAM_NAME, Record={"Data": test_data_enc}
+    )
+    logging.info(json.dumps(response, indent=2))
